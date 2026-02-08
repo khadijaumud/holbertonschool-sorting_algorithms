4342ce14ff307684f33723c02071dab22e484226
khadijaumud <kh.umud07@gmail.com>
2026-02-08 10:52:42 +0000

    fix 3rd task

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,7 +8,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t i, j, low, high, p;
+	size_t i, j, low, high;
 	int temp;
 
 	if (array == NULL || size < 2)
@@ -17,10 +17,8 @@ void quick_sort(int *array, size_t size)
 	low = 0;
 	high = size - 1;
 
-	
 	if (low < high)
 	{
-		
 		i = low;
 		for (j = low; j < high; j++)
 		{
@@ -43,7 +41,6 @@ void quick_sort(int *array, size_t size)
 			array[high] = temp;
 			print_array(array, size);
 		}
-		
 		if (i > 0)
 			quick_sort(array, i);
 		quick_sort(array + i + 1, size - i - 1);
